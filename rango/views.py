cc8f2afae6ef5b066b845b3e507862d857688685
Minhao Han <hannidiot2018@gmail.com>
2022-01-28 00:47:16 +0000

    add templates, create index.html

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest):
-    return HttpResponse('Rango says hey there partner!<a href="/rango/about/">About</a>')
+    context_dict = {
+        "boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"
+    }
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request: HttpRequest):
     return HttpResponse('Rango says here is the about page.<a href="/rango/">Index</a>')
